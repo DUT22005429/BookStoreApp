b8fd4120f7194168647fcc502507c5e4a592e948
DUT22005429 <22005429@dut4life.ac.za>
2022-07-12 12:56:56 +0200

    Add project files.

---
+++ Product Catergories.sql
@@ -0,0 +1,12 @@
+SET IDENTITY_INSERT [dbo].[ProductCatergories] ON
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (1, N'IT')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (2, N'BIO')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (3, N'MATH')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (4, N'ENG')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (5, N'LO')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (6, N'HIST')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (7, N'GEO')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (8, N'MATH LIT')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (9, N'AFR')
+INSERT INTO [dbo].[ProductCatergories] ([ProductCatergoryId], [CatergoryName]) VALUES (10, N'ZULU')
+SET IDENTITY_INSERT [dbo].[ProductCatergories] OFF
